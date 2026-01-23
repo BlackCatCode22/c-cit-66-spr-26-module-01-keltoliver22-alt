4f253a43443a17fe12d508921fe34cb0b0846a11
keltoliver22-alt <keltoliver22@gmail.com>
2026-01-22 23:41:37 -0800

    Add files via upload

---
+++ doubleInt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int userNumber;
+
+    cout << "Enter an integer: ";
+    cin >> userNumber;
+
+    int doubled = userNumber * 2;
+
+    cout << "Doubled value: " << doubled << endl;
+
+    return 0;
+}
